a063b87c00822afa700175c4b9b28a4ebd4b9047
Albert Liu <albertymliu@gmail.com>
2018-03-12 23:07:27 -0400

    server trying to connect

--- src/server/Server.java
+++ src/server/Server.java
@@ -1,11 +1,40 @@
 package server;
 
-import java.util.ArrayList;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class Server {
-	private ArrayList<Integer> hashList;
+
+	public static void main(String[] args) {
+		
+		String url = "jdbc:mysql://192.168.1.5:3306/javabase";
+		String username = "java";
+		String password = "java";
+
+		System.out.println("Connecting database...");
+
+		try {
+		    Class.forName("com.mysql.jdbc.Driver");
+		    System.out.println("Driver loaded!");
+		} catch (ClassNotFoundException e) {
+		    throw new IllegalStateException("Cannot find the driver in the classpath!", e);
+		}
+		
+		try (Connection connection = DriverManager.getConnection(url, username, password)) {
+		    System.out.println("Database connected!");
+		} catch (SQLException e) {
+		    throw new IllegalStateException("Cannot connect the database!", e);
+		}
+		
+		
+	}
 	
 	
-	//https://github.com/speedment/speedment/wiki/Tutorial:-Build-a-Social-Network
-	//https://stackoverflow.com/questions/2839321/connect-java-to-a-mysql-database
+	
 }
+
+
+
+//https://github.com/speedment/speedment/wiki/Tutorial:-Build-a-Social-Network
+	//https://stackoverflow.com/questions/2839321/connect-java-to-a-mysql-database
