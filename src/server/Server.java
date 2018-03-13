b041410edf69b67303ebf7d97fd9502464bf7df4
Albert Liu <albertymliu@gmail.com>
2018-03-12 23:10:00 -0400

    configured SQL connector jar file

--- src/server/Server.java
+++ src/server/Server.java
@@ -14,13 +14,13 @@ public class Server {
 
 		System.out.println("Connecting database...");
 
-		try {
+		/*try {
 		    Class.forName("com.mysql.jdbc.Driver");
 		    System.out.println("Driver loaded!");
 		} catch (ClassNotFoundException e) {
 		    throw new IllegalStateException("Cannot find the driver in the classpath!", e);
 		}
-		
+		*/
 		try (Connection connection = DriverManager.getConnection(url, username, password)) {
 		    System.out.println("Database connected!");
 		} catch (SQLException e) {
