fff2e58978867e3e2c8e3e817e0d5f2c3a4f598c
Albert Liu <albertymliu@gmail.com>
2018-03-12 23:15:45 -0400

    removed useless try catch from debugging

--- src/server/Server.java
+++ src/server/Server.java
@@ -13,28 +13,16 @@ public class Server {
 		String password = "java";
 
 		System.out.println("Connecting database...");
-
-		/*try {
-		    Class.forName("com.mysql.jdbc.Driver");
-		    System.out.println("Driver loaded!");
-		} catch (ClassNotFoundException e) {
-		    throw new IllegalStateException("Cannot find the driver in the classpath!", e);
-		}
-		*/
+		
 		try (Connection connection = DriverManager.getConnection(url, username, password)) {
 		    System.out.println("Database connected!");
 		} catch (SQLException e) {
 		    throw new IllegalStateException("Cannot connect the database!", e);
 		}
 		
-		
 	}
-	
-	
 	
 }
 
-
-
 //https://github.com/speedment/speedment/wiki/Tutorial:-Build-a-Social-Network
-	//https://stackoverflow.com/questions/2839321/connect-java-to-a-mysql-database
+//https://stackoverflow.com/questions/2839321/connect-java-to-a-mysql-database
