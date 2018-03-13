3b7aadf111511df9ba23df7977a4b9debb87cf7c
Albert Liu <albertymliu@gmail.com>
2018-03-12 23:36:33 -0400

    added methods according to resource 3

---
+++ src/users/NewAccount.java
@@ -0,0 +1,107 @@
+package users;
+
+import java.io.Serializable;
+import java.util.Date;
+
+public abstract class NewAccount implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	private Long id;
+	private String username;
+	private String email;
+	private String first;
+	private String last;
+	private Date birthday;
+	
+	public Long getId() {
+		return id;
+	}
+	
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+	
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	
+	public String getEmail() {
+		return email;
+	}
+	
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	public String getFirst() {
+		return first;
+	}
+	
+	public void setFirst(String first) {
+		this.first = first;
+	}
+	
+	public String getLast() {
+		return last;
+	}
+	
+	public void setLast(String last) {
+		this.last = last;
+	}
+	
+	public Date getBirthday() {
+		return birthday;
+	}
+	
+	public void setBirthday(Date birthday) {
+		this.birthday = birthday;
+	}
+	
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+	
+	  /**
+     * The user ID is unique for each User. So this should compare User by ID only.
+     * @see java.lang.Object#equals(java.lang.Object)
+     */
+    @Override
+    public boolean equals(Object other) {
+        return (other instanceof User) && (id != null)
+             ? id.equals( ( (User) other).getId() )
+             : (other == this);
+    }
+
+    /**
+     * The user ID is unique for each User. So User with same ID should return same hashcode.
+     * @see java.lang.Object#hashCode()
+     */
+    @Override
+    public int hashCode() {
+        return (id != null) 
+             ? (this.getClass().hashCode() + id.hashCode()) 
+             : super.hashCode();
+    }
+
+    /**
+     * Returns the String representation of this User. Not required, it just pleases reading logs.
+     * @see java.lang.Object#toString()
+     */
+    @Override
+    public String toString() {
+        return String.format("User[id=%d,email=%s,firstname=%s,lastname=%s,birthdate=%s]", 
+        					id, email, first, last, birthday);
+    }
+	
+	
+	
+	
+}
