79367e84d617d551bbf562012a6c9a67808d99f3
Albert Liu <albertymliu@gmail.com>
2018-03-12 21:13:31 -0400

    added password object

---
+++ src/server/Password.java
@@ -0,0 +1,17 @@
+package server;
+
+public class Password {
+	private String password;
+	
+	public Password(String p) {
+		password = p;
+	}
+	
+	public String getPassword() {
+		return this.password;
+	}
+	
+	public static String getPreset() {
+		return "java";
+	}
+}
