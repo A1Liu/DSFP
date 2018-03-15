1e6de5d4eb538d9e4494d016d7540d6b836f7a73
Albert Liu <albertymliu@gmail.com>
2018-03-14 23:33:48 -0400

    reformatted methods to be generic for flexibility

--- src/network/NodeNetwork.java
+++ src/network/NodeNetwork.java
@@ -1,16 +1,28 @@
 package network;
 
-public class NodeNetwork<E> extends Vertex {
+public class NodeNetwork<E> extends Network<Node<E>> {
+	
+}
+
+class Node<E> extends Vertex {
 	
 	private String name;
 	private E data;
 	
-	NodeNetwork() {
-
+	Node() {
+		this(0,null,null);
 	}
 	
-	NodeNetwork(String n, E e) {
-		super();
+	Node(int i) {
+		this(i,null,null);
+	}
+	
+	Node(int i, E e) {
+		this(0,null,e);
+	}
+	
+	Node(int i,String n, E e) {
+		super(i);
 		name = n;
 		data = e;
 	}
