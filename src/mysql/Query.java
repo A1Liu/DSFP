c187add87368ec1af79041ac441191a49837be77
Albert Liu <albertymliu@gmail.com>
2018-03-19 21:41:47 -0400

    sql

--- src/mysql/Query.java
+++ src/mysql/Query.java
@@ -10,7 +10,8 @@ import users.User;
 
 public class Query {
 	
-	Connection connection;
+	private Connection connection;
+	private Statement statement;
 	
 	public Query(Connection c) {
 		connection = c;
@@ -24,12 +25,19 @@ public class Query {
 		return null;
 	}
 	
+	public <E> ArrayList<E> getResults() {
+		
+	}
 	
+	public <E> E getResultRow(int row) {
+		
+	}
 	
 	public void hello() throws SQLException {
 	
-	Statement statement = connection.createStatement();//makes a statement object
-
+	statement = connection.createStatement();//makes a statement object
+	
+	
 	
     // Step 3: Execute a SQL SELECT query, the query result
     //  is returned in a 'ResultSet' object.
@@ -41,13 +49,11 @@ public class Query {
 
     // Step 4: Process the ResultSet by scrolling the cursor forward via next().
     System.out.println("The records selected are:");
-    int rowCount = 0;
     while(results.next()) {   // Move the cursor to the next row, return false if no more row
        String title = results.getString("title");//gets information from the result of the query. The string is the column name
        double price = results.getDouble("price");
        int    qty   = results.getInt("qty");
        System.out.println(title + ", " + price + ", " + qty);
-       ++rowCount;
     }
 	
 }
