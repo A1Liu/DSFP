bb1b74af0c1bffcd6cef2942f18210e0df2173c0
Albert Liu <albertymliu@gmail.com>
2018-04-09 01:13:37 -0400

    tried to start working on server logic

--- src/server/Commands.java
+++ src/server/Commands.java
@@ -1,22 +1,36 @@
 package server;
 
 import dao.DAOException;
+import sql.DAOFactory;
+import sql.PassDAO;
+import sql.UserDAO;
 
 
 class Commands {
 	
-	private Commands() {
-		
+	DAOFactory javabase;
+	UserDAO userDAO;
+	PassDAO passDAO;
+	
+	void execute(String command) {
+		execute(command.split(" "));
 	}
 	
-	static void execute(String command) throws IllegalArgumentException, DAOException {
+	void execute(String[] command) throws IllegalArgumentException, DAOException {
 		
-		switch(command) {
-		case "start":
+		switch(command[0]) {
+		case "start"://start server. First connect to DB, then open to user connections
+			javabase = DAOFactory.getInstance("javabase.jdbc");
+	        System.out.println("DAOFactory successfully obtained: " + javabase);
+	        userDAO = javabase.getUserDAO();
+	        System.out.println("UserDAO successfully obtained: " + userDAO);
+	        passDAO = javabase.getPassDAO();
+	        System.out.println("PassDAO successfully obtained: " + passDAO);
 			break;
-		case "stop":
+		case "stop"://stop server. should also save all working memory and stuff like that to DB before closing connection with DB
 			break;
 		default:
+			throw new IllegalArgumentException("that's not a valid command!");
 		}
 	}
 	
--- src/server/Server.java
+++ src/server/Server.java
@@ -7,14 +7,33 @@ package server;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
-public class Server {
+import dao.DAOException;
+import sql.DAOFactory;
+import sql.PassDAO;
+import sql.UserDAO;
 
+public class Server {
+	
+	DAOFactory javabase;
+	UserDAO userDAO;
+	PassDAO passDAO;
+	String driverName;
+	ThreadHandler userConnections;
+	int port;
+	
+	Server(String driverName, int port) {
+		this.driverName = driverName;
+		this.port = port;
+	}
+	
     public static void main(String[] args) throws Exception {
         
+    	Server server = new Server("javabase.jdbc", 1100);
+    	
     	int port = 1100;
         System.out.println( "Start server on port: " + port );
         
-        ThreadHandler server = new ThreadHandler( port );
+        ThreadHandler threads = new ThreadHandler( port );
         
 		BufferedReader consoleLine = new BufferedReader(new InputStreamReader(System.in));
         boolean on = true;
@@ -24,11 +43,11 @@ public class Server {
         	
         	switch (input) {
         	case "start":
-        		server.startServer();
+        		threads.startServer();
         		System.out.println("Server Started!");
         		break;
         	case "stop":
-        		server.stopServer();
+        		threads.stopServer();
         		System.out.println("Server Stopped.");
         		on = false;
         		break;
@@ -48,4 +67,27 @@ public class Server {
         fillDB("lib/userList.txt", userDAO, passDAO);
         */
     }
+    
+    void execute(String command) {
+		execute(command.split(" "));
+	}
+	
+	void execute(String[] command) throws IllegalArgumentException, DAOException {
+		
+		switch(command[0]) {
+		case "start"://start server. First connect to DB, then open to user connections
+			javabase = DAOFactory.getInstance(driverName);
+	        System.out.println("DAOFactory successfully obtained: " + javabase);
+	        userDAO = javabase.getUserDAO();
+	        System.out.println("UserDAO successfully obtained: " + userDAO);
+	        passDAO = javabase.getPassDAO();
+	        System.out.println("PassDAO successfully obtained: " + passDAO);
+	        
+			break;
+		case "stop"://stop server. should also save all working memory and stuff like that to DB before closing connection with DB
+			break;
+		default:
+			throw new IllegalArgumentException("that's not a valid command!");
+		}
+	}
 }
