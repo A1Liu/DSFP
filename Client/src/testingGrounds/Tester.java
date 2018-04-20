48201218125b8de7c54d237173d7c312951ca6a1
Albert Liu <albertymliu@gmail.com>
2018-04-20 09:33:35 -0400

    client doesn't need server code

--- Client/src/testingGrounds/Tester.java
+++ Client/src/testingGrounds/Tester.java
@@ -1,21 +1,10 @@
 package testingGrounds;
 
-import network.NodeNetwork;
 
 public class Tester {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		NodeNetwork<Integer> albert = new NodeNetwork<Integer>();
-		albert.addVertex("hello");
-		albert.addVertex("hi");
-		albert.addVertex("boi");
-		albert.addVertex("spoopy");
-		albert.addVertex("poop");
-		albert.addEdge("hello", "hi",3);
 
-		String x = albert.keys().toString();
-		System.out.println(x);
 	}
 
 }
