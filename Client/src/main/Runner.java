681c9e08760714eb8647ad55b483970a4c11bdb7
Albert Liu <albertymliu@gmail.com>
2018-04-24 11:17:53 -0400

    added main package"

---
+++ Client/src/main/Runner.java
@@ -0,0 +1,26 @@
+package main;
+
+import gui.LoginPage;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Runner extends Application {
+	
+	
+	
+	public static void main(String... args) {
+		launch(args);
+	}
+
+	@Override
+    public void start(Stage primaryStage) {
+        
+		primaryStage.setTitle("JavaFX Welcome");
+        
+        Scene scene = new LoginPage();
+        primaryStage.setScene(scene);
+        
+        primaryStage.show();
+    }
+}
