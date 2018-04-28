4e9045cd910221a9683c32dff7d4ff7d2bd08eb2
Albert Liu <albertymliu@gmail.com>
2018-04-28 17:11:48 -0400

    finished very rough error screen

--- Client/src/gui/ErrorScreen.java
+++ Client/src/gui/ErrorScreen.java
@@ -1,21 +1,38 @@
 package gui;
 
 import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Paint;
+import javafx.scene.shape.Rectangle;
 import javafx.scene.text.*;
 
 public class ErrorScreen extends GridPane {
 
 	private static Text message;
+	private static Rectangle box;
+	private static StackPane container;
 	
 	static {
-		message = new Text("Error");
 		
+		box = new Rectangle(300, 250, Paint.valueOf("0000ff"));
+		container = new StackPane();
+		container.getChildren().add(box);
+		
+		message = new Text("Error");
+		message.setFont(new Font(100));
+		container.getChildren().add(message);
 	}
 	
 	public ErrorScreen() {
+		this.setup();
+	}
+	
+	private void setup() {
+		this.setAlignment(Pos.CENTER);
 		this.setPadding(new Insets(25,25,25,25));
-		this.add(message,0,0);
+		this.add(container,0,0);
 	}
 	
 }
