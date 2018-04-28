c5bb8b1c5cf2c679099ef2dfeac43036a87a818d
Albert Liu <albertymliu@gmail.com>
2018-04-28 18:08:21 -0400

    changed page stuff to make subclasses little simpler

---
+++ client/src/gui/HomePage.java
@@ -0,0 +1,24 @@
+package gui;
+
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.StackPane;
+
+import static util.Const.HEIGHT;
+import static util.Const.WIDTH;
+
+class HomePage extends Page {
+
+	private final static StackPane container;
+	private final static GridPane grid;
+	
+	static {
+		grid = new GridPane();
+		container = new StackPane();
+		container.getChildren().add(grid);
+	}
+	
+	public HomePage(Controller c) {
+		super(container, c);
+	}
+
+}
