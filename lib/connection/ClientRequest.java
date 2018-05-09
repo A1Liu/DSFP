6601c1d976f9f5baef6a6a2e841fcd55053ccbd3
Albert Liu <albertymliu@gmail.com>
2018-05-08 21:25:08 -0400

    added stuff to connection package in library. Slowly building the connection between client and server

---
+++ lib/connection/ClientRequest.java
@@ -0,0 +1,15 @@
+package connection;
+
+public class ClientRequest extends Packet<Byte> {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public ClientRequest(double label, Byte data) {
+		super(label, data);
+		// TODO Auto-generated constructor stub
+	}
+
+}
