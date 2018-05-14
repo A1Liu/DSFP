1e4b124b490043d2f1f9ed85019e90e8f7cb3a3e
Albert Liu <albertymliu@gmail.com>
2018-05-13 23:28:15 -0400

    sql planning

--- Server/SQL/tables/Users.sql
+++ Server/SQL/tables/Users.sql
@@ -1,7 +1,8 @@
 /*\. code/java/dsfp/server/SQL/Users.sql*/
 USE javabase;
-DROP TABLE javabase.passwords;
-DROP TABLE javabase.users;
+/*DROP TABLE javabase.passwords;
+DROP TABLE javabase.sessions;
+DROP TABLE javabase.users;*/
 
 CREATE TABLE javabase.users (
 	id BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
@@ -22,6 +23,16 @@ CREATE TABLE javabase.passwords (
 	FOREIGN KEY (id) references users (id),
 	PRIMARY KEY (id)
   );
+
+CREATE TABLE javabase.sessions (
+	sessionNum BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
+	userID BIGINT UNSIGNED NOT NULL,
+	sessionID BIGINT UNSIGNED NOT NULL,
+	dateCreated DATE NULL,
+	FOREIGN KEY (userID) REFERENCES users (id),
+	UNIQUE (sessionID),
+	PRIMARY KEY (sessionNum)
+);
 /*
 	INSERT INTO user (username, email, firstname, lastname) VALUES("l","e","rt","ed");
 	INSERT INTO passwords (id, salt,password) VALUES(1,12,"hello");*/
