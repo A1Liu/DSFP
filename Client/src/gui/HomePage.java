11cef6e7549cc9bf00d8acd57ec7839bf507f7bc
Albert Liu <albertymliu@gmail.com>
2018-05-14 18:24:51 -0400

    trying to fix git indices

---
+++ Client/src/gui/HomePage.java
@@ -0,0 +1,12 @@
+package gui;
+
+import javafx.scene.Parent;
+
+public class HomePage extends Page {
+
+	public HomePage(Parent root, Controller control) {
+		super(root, control);
+		// TODO Auto-generated constructor stub
+	}
+
+}
